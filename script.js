7ac91b104a4956706a18d908d909fd5d701afaa5
Vignesh <vignesh1410s@gmail.com>
2024-07-26 00:32:05 -0700

    added new

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-function update() {
+// function update() {
     
-    return Math.floor(Math.random() * 10); 
-}
+//     return Math.floor(Math.random() * 10); 
+// }
 
-function updateLevel() {
-    const MeasureLevel = document.getElementById('circle1');
-    const Level = update();
+// function updateLevel() {
+//     const MeasureLevel = document.getElementById('circle1');
+//     const Level = update();
 
-    MeasureLevel.textContent = Level;
+//     MeasureLevel.textContent = Level;
 
-    if (Level > 0) {
-        MeasureLevel.style.backgroundColor = 'green';
-        MeasureLevel.style.boxShadow = '0 0 20px 5px rgba(0, 255, 0, 0.3)';
-    } else {
-        MeasureLevel.style.backgroundColor = 'red';
-        MeasureLevel.style.boxShadow = '0 0 10px 5px rgba(255, 0, 0, 0.3)';
-    }
-}
+//     if (Level > 0) {
+//         MeasureLevel.style.backgroundColor = 'green';
+//         MeasureLevel.style.boxShadow = '0 0 20px 5px rgba(0, 255, 0, 0.3)';
+//     } else {
+//         MeasureLevel.style.backgroundColor = 'red';
+//         MeasureLevel.style.boxShadow = '0 0 10px 5px rgba(255, 0, 0, 0.3)';
+//     }
+// }
 
 
-setInterval(updateLevel, 1000);
+// setInterval(updateLevel, 1000);
